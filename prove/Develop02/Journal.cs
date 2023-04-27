38ed34f0e87f1293ca2a6406b70443f3d2775a2d
nataliehamel <Nataliestoker5@gmail.com>
2023-04-26 20:03:50 -0700

    some more progrees on prove assignment

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -3,19 +3,7 @@ class Journal
 {
     List<Entry> entries;
 
-    List<string> prompts = new List<string>
-    {
-        "When were you the happiest today and why?",
-        "If you could redo one thing today, what would it be?",
-        "How did you treat yourself today?",
-        "What did you do spiritually today?",
-        "What are you grateful for?",
-        "How did you serve someone today?",
-        "Who was the most interesting person I intereacted with today?",
-        "What was the best part of my day?",
-        "How did I see the hand of the Lord in my life today?",
-        "What was the strongest emotion I felt today?"
-    };
+    List<string> prompts;
 
     public Journal()
     {
@@ -48,5 +36,6 @@ class Journal
     public string GetRandomPrompt()
     {
         return "";
+        
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -18,6 +18,26 @@ class Program
                 "What would you like to do? "
             };
 
+        
+        var random = new Random();
+        List<string> prompts = new List<string>
+            {
+                "When were you the happiest today and why?",
+                "If you could redo one thing today, what would it be?",
+                "How did you treat yourself today?",
+                "What did you do spiritually today?",
+                "What are you grateful for?",
+                "How did you serve someone today?",
+                "Who was the most interesting person I intereacted with today?",
+                "What was the best part of my day?",
+                "How did I see the hand of the Lord in my life today?",
+                "What was the strongest emotion I felt today?"
+            };
+        int index = random.Next(prompts.Count);
+        Console.WriteLine(prompts[index]);
+
+
+
         while (menuUserInput != 5)
         {
             foreach(string menuItem in menu)
@@ -25,13 +45,9 @@ class Program
                 Console.WriteLine(menuItem);
             }
             menuUserInput = int.Parse(Console.ReadLine());
+
         }
-
-
-
-
-
-
+        
         DateTime theCurrentTime = DateTime.Now;
         string dateText = theCurrentTime.ToShortDateString();
 
