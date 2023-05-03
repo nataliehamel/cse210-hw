c9b09d6786b36271c6c61129752d1ebb7f4a7afe
nataliehamel <Nataliestoker5@gmail.com>
2023-05-03 14:54:05 -0700

    scripture memorizer program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference scriptureReference = new Reference("Proverbs", "3", "5", "6");
+        Scripture scripture = new Scripture(scriptureReference, "Trust in the lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+        string userInput = "";
+
+        while (userInput != "quit" && scripture.HasWordsLeft() == true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetScripture());
+            Console.WriteLine();
+            Console.WriteLine("Press enter to contine or 'quit' to finish: ");
+            userInput = Console.ReadLine();
+            scripture.RemoveWords();
+        }
+        Console.WriteLine(scripture.GetScripture());            
+        Console.WriteLine();
+        Console.WriteLine("I hope you enjoyed this memorizing process and hopefully it worked!");
     }
 }
